6a79d0827bfa970855a3a9424b9cd2646b185a08
Noh Sung Gyu <gkdlshtjdrb@naver.com>
2025-02-28 11:12:12 +0900

    docker compose env script 작성성

---
+++ docker-compose/install/docker-env-install.sh
@@ -0,0 +1,27 @@
+echo '======= [1] uninstall old versions =========='
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+echo '======= [2] Set up the repository =========='
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+echo '======= [3] Install the Docker packages =========='
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo '======= [4] Start Docker Engine =========='
+sudo systemctl enable --now docker
+
+echo '======= [5] Manage Docker as a non-root user =========='
+sudo usermod -aG docker $USER
+newgrp docker
+
+echo '======= [6] Configure Docker to start on boot with systemd =========='
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
