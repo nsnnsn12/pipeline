afdfa0b49a9f1c96056276300160d1a55249dc99
Noh Sung Gyu <gkdlshtjdrb@naver.com>
2025-03-25 15:51:42 +0900

    깃허브 액션을 이용한 docker swarm 배포용 서버 세팅 스크립트

---
+++ github-action/setting-remote-server.sh
@@ -0,0 +1,37 @@
+echo '======= [1] uninstall old versions =========='
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+echo '======= [2] Set up the repository =========='
+sudo dnf -y install dnf-plugins
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+echo '======= [3] Install the Docker packages =========='
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo '======= [4] Start Docker Engine =========='
+sudo systemctl enable --now docker
+
+echo '======= [5] Manage Docker as a non-root user =========='
+sudo usermod -aG docker $USER
+newgrp docker
+
+echo '======= [6] Configure Docker to start on boot with systemd =========='
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+echo '======= [7] Docker Swarm Init =========='
+sudo docker swarm init
+
+echo '======= [8] Install GitHub CLI =========='
+sudo dnf install 'dnf-command(config-manager)'
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf install gh --repo gh-cli
+
+echo '======= [9] You have to set SSH For Deploy =========='
